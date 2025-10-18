2618ade228841fb243c1ee01f3bca5bbdebef7c8
Anton Smirnov <antony01s@mail.ru>
2025-10-18 13:28:59 +0300

    edit build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,11 @@
 BASEDIR=$(realpath "$(dirname "$0")")
 ROOTDIR=$(realpath "$BASEDIR/..")
 
+if [ -d "$ROOTDIR/build" ]; then
+    rm -rf "$ROOTDIR/build"
+fi
+
+mkdir -p "$ROOTDIR/build"
+
 cmake -S "$ROOTDIR" -B "$ROOTDIR/build"
 cmake --build "$ROOTDIR/build"
